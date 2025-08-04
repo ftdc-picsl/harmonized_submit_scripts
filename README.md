3c3028136535ba3b464a5ba7988fa86dc6d69bab
Christopher Olm <caugolm@gmail.com>
2025-08-04 13:43:23 -0400

    init

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# harmonized_submit_scripts
-'harmonized' scripts for ADRC and FTDC t1 processing
+8/4/2025
+This directory contains the 'harmonized' scripts for the ADRC and FTDC to submit T1 (and eventually hopefully other modalities) data for processing
+We use the containers in /project/ftdc_pipeline/ftdc-picsl
+For this version, we use 
+    -pmacsT1wPreprocessing-0.5.1 for brain masking with the neck trim
+    -pmacsAntsnetct-0.2.2 for the segmentation and cortical thickness estimate
+...put the links here, chris
+
+
---
+++ submit_pmacsT1wPreprocessing-0.5.1.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+bids_in="/project/ftdc_volumetric/fw_bids"
+t1pre_out="/project/ftdc_pipeline/data/T1wPreprocessing_051"
+
+submit_script="/project/ftdc_pipeline/ftdc-picsl/pmacsT1wPreprocessing-0.5.1/bin/submit_preproc.sh"
+
+
+if [[ $# -lt 2 ]] ; then 
+	echo "USAGE: ./submit_pmacsT1Preprocessing-0.5.1.sh <sub,seslist.txt,csv> <session or participant>"
+	echo "uses hd-bet to make a brain mask and then neck trim the T1w image" 
+	echo " 		<sub,ses.csv> requires a subID,sesID per line if session"  
+	echo " 		<sub.txt> requires a subID per line if participant"
+	echo "" 
+	echo "this script submits data in list to submit_preproc.sh in $scriptsdir "
+	echo " images are looked for in BIDS format in $bids_in "
+	echo " output in BIDS-ish format goes to $t1pre_out"
+	exit 1
+fi
+
+sublist=$1
+listtype=$2
+
+
+if [[ $listtype != "session" && $listtype != "participant" ]] ; then
+	echo "list type must be 'session' or 'participant'"
+	exit 1
+fi	
+
+
+${submit_script} -i ${bids_in} -o ${t1pre_out} ${sublist} ${listtype}
