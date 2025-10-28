02af2f6523c81093e5451a88af3d12b30f02805e
hamsiradhakrishnan <hamsanandiniradhakrishnan@gmail.com>
2025-10-28 14:52:06 -0400

    Updated preproc script to use pmacsT1wPreprocessing-0.6.0

--- submit_pmacsT1wPreprocessing.sh
+++ submit_pmacsT1wPreprocessing.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-submit_script="/project/ftdc_pipeline/ftdc-picsl/pmacsT1wPreprocessing-0.5.1/bin/submit_preproc.sh"
+submit_script="/project/ftdc_pipeline/ftdc-picsl/pmacsT1wPreprocessing-0.6.0/bin/submit_preproc.sh" #version changed on 2025-10-28 to 0.6.0
 
 if [[ $# -lt 3 ]] ; then 
-	echo "USAGE: ./submit_pmacsT1Preprocessing-0.5.1.sh <sub,seslist.txt,csv> <session or participant> <config>"
+	echo "USAGE: ./submit_pmacsT1Preprocessing.sh <sub,seslist.txt,csv> <session or participant> <config>"
 	echo "uses hd-bet to make a brain mask and then neck trim the T1w image" 
 	echo " 		<sub,ses.csv> requires a subID,sesID per line if session"  
 	echo " 		<sub.txt> requires a subID per line if participant"
 	echo "		<config> is the config file used to define the base directories for bids_in, T1Preprocessing output: t1pre_dir, (and ANTsNetCT output: antsnetct_dir)"
 	echo "" 
-	echo "this script submits data in list to submit_preproc.sh in $scriptsdir "
+	echo "this script submits data in list to $submit_script "
 	exit 1
 fi
 
@@ -24,6 +24,6 @@ fi
 
 source ${config}
 
-cmd="${submit_script} -i ${bids_in} -o ${t1pre_dir} ${sublist} ${listtype} --trim-neck" 
+cmd="${submit_script} -i ${bids_in} -o ${t1pre_dir} ${sublist} ${listtype}" # --trim-neck is default now
 echo $cmd
 $cmd
